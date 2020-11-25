17a4227923acee42e04bb0f2edf4451b90b27871
hibuz <hibuz@hibuz.com>
2020-11-26 03:37:36 +0900

    Add market cap data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StockList from './components/StockList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Top 20 Companies in the world by Market Cap Today</h2>
+      <StockList/>
     </div>
-  );
+  )
 }
 
 export default App;
