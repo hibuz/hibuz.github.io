9ad49681418f96ebcab442968a59c872d911f062
hibuz <hibuz@hibuz.com>
2020-11-29 18:58:24 +0900

    Refactor company name

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import StockList from './components/StockList';
 
 const App = () => {
+  const [stockList, setStockList] = useState(null);
+
+  function fetchStockData() {
+    fetch('https://api.fureweb.com/spreadsheets/1aXzns9mgucWf2hv0nORnFqmN6KuxNl9FTFXjIHNYeg4')
+      .then((res) => res.json())
+      .then(res => {
+        const data = res.data
+          .sort((a: any, b: any) => a.id > b.id ? 1 : -1)
+          .slice(0, 20);
+          setStockList(data)
+    });
+  }
+
+  useEffect(() => {
+    fetchStockData();
+  }, []);
+
   return (
     <div>
       <h2>전세계 기업 시가총액 Top 20 순위</h2>
-      <StockList/>
+      {stockList != null && <StockList rows={stockList}/>}
     </div>
   )
 }
--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -1,45 +1,51 @@
 import React from 'react';
-import axios from 'axios';
-import { DataGrid, ColDef } from '@material-ui/data-grid';
+import { DataGrid, ColDef, CellParams, RowParams } from '@material-ui/data-grid';
 
 const columns: ColDef[] = [
-    { field: 'id', headerName: '순위', width: 70 },
-    { field: 'name_ko', headerName: '기업', width: 160},
-    { field: 'currency', headerName: '통화', width: 70 },
-    {
-      field: 'market_cap_ko',
-      headerName: '시총(KRW)',
-      width: 150,
-      sortable: false,
-      valueFormatter: ({value}) => value + ' 조원'
+  { field: 'id', headerName: '순위', width: 60 },
+  { field: 'name_ko',
+    headerName: '기업',
+    width: 140,
+    renderCell: (params: CellParams) => (
+      <Company name={params.value} logo={params.getValue('logo')} />
+    )
+  },
+  { field: 'currency', headerName: '통화', width: 70 },
+  {
+    field: 'market_cap_ko',
+    headerName: '시총(KRW)',
+    description: '실시간 원화 환율 적용',
+    width: 150,
+    sortable: false,
+    valueFormatter: ({value}) => value + ' 조원'
   },
   {
-        field: 'market_cap_en',
-        headerName: '시총(USD)',
-        width: 150,
-        sortable: false,
-        valueFormatter: ({value}) => value + ' 억달러'
+    field: 'market_cap_en',
+    headerName: '시총(USD)',
+    description: '실시간 달러 환율 적용',
+    width: 150,
+    sortable: false,
+    valueFormatter: ({value}) => value + ' 억달러'
   }
 ];
 
-export default class StockList extends React.Component {
-    state = {
-      rows: []
-    }
-  
-    componentDidMount() {
-      axios.get(`https://api.fureweb.com/spreadsheets/1aXzns9mgucWf2hv0nORnFqmN6KuxNl9FTFXjIHNYeg4`)
-        .then(res => {
-          const rows = res.data.data
-          .sort((a: any, b: any) => a.id > b.id ? 1 : -1)
-          .slice(0, 20);
-          this.setState({ rows });
-        })
-    }
-  
-    render() {
-      return (
-        <DataGrid rows={this.state.rows} columns={columns} hideFooter={true} autoHeight={true} />
-      )
-    }
+function Company(props: any){
+
+  let img = '' + props.logo;
+  if (!img.startsWith('http')) {
+    img = 'https://' + props.logo + '/favicon.ico';
   }
+  return <div><img width="15" src={img} alt={props.name}/> <span> {props.name}</span></div>
+};
+
+export default function StockList({rows}: any) {
+    const onRowClick = (param: RowParams) => {
+      window.open('https://www.google.com/search?q=' + param.getValue('exchange') + ':' + param.getValue('symbol') + '&tbm=fin', 'google_fin');
+    }
+
+    return (
+      <div style={{ height: 1000, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns} onRowClick={onRowClick} hideFooter autoHeight />
+      </div>
+    )
+}
