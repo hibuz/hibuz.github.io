5dc7a413dab530e34e286210dfdbd9b9c48a035f
hibuz <hibuz@hibuz.com>
2020-11-29 11:12:51 +0900

    Fix style

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import StockList from './components/StockList';
 const App = () => {
   return (
     <div>
-      <h2>Top 20 Companies in the world by Market Cap Today</h2>
+      <h2>전세계 기업 시가총액 Top 20 순위</h2>
       <StockList/>
     </div>
   )
